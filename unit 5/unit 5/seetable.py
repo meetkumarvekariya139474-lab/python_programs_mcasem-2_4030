53f490c66ca5580f6f62c76ece48131185117ff7
meetkumarvekariya139474-lab <meetkumar.vekariya139474@marwadiuniversity.ac.in>
2026-04-09 11:24:13 +0530

    Delete unit 5/unit 5 directory

--- unit 5/unit 5/seetable.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("students.db")
-cur = conn.cursor()
-
-cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
-print(cur.fetchall())
-
-conn.close()
